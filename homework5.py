528d9f6b3704f833e5a3482fcaa72d3338004d20
Aleksey236 <strelkovaleksej208@gmail.com>
2024-07-28 13:26:25 +0400

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,9 @@
+immutable_var = 1, 2, True, 'Urban'
+print(immutable_var)
+# immutable_var [0] = 100 # кортеж относится к неизменяемым типам данных
+mutable_list = [1, 3, True, 'Urban']
+print(mutable_list)
+mutable_list.append(False)
+print(mutable_list)
+mutable_list.remove(1)
+print(mutable_list)
